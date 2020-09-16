933cfea183a7c26f596e37cd2a02220000dd78a2
Daniel Palmer <daniel@0x0f.com>
2020-09-16 21:26:54 +0900

    generic board

--- board/mstar/mstarv7/mstarv7.c
+++ board/mstar/mstarv7/mstarv7.c
@@ -2,13 +2,16 @@
  * Mstar Armv7 generic board
  */
 
-DECLARE_GLOBAL_DATA_PTR;
+#include <init.h>
+#include <fdtdec.h>
+
+#ifdef CONFIG_DEBUG_UART
+#include <debug_uart.h>
+#endif
 
 #ifdef CONFIG_SPL_BUILD
 void board_init_f(ulong dummy)
 {
-	uint32_t cpuid;
-	int chiptype = mstar_chiptype();
 #ifdef CONFIG_DEBUG_UART
 	debug_uart_init();
 #endif
