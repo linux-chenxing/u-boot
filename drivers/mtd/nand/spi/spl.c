5c8bc6a73dc3ce51d15cde8d8947f9f132a94ada
Daniel Palmer <daniel@0x0f.com>
2021-06-19 11:51:49 +0900

    mtd: nand: spi: some cleaning up

--- drivers/mtd/nand/spi/spl.c
+++ drivers/mtd/nand/spi/spl.c
@@ -4,18 +4,18 @@
 static struct udevice *dev;
 static struct mtd_info *mtd;
 
-int spl_spinand_peb_size()
+int spl_spinand_peb_size(void)
 {
 	return mtd->erasesize;
 }
 
-int spl_spinand_init()
+int spl_spinand_init(void)
 {
 	int ret;
 
 	ret = uclass_get_device_by_driver(UCLASS_MTD, DM_GET_DRIVER(spinand), &dev);
 	if (ret){
-		printf("Failed to probe spi-nand: %d\n", ret);
+		//printf("Failed to probe spi-nand: %d\n", ret);
 		return ret;
 	}
 
@@ -30,5 +30,11 @@ int spl_spinand_read_block(int block, int offset, int len, void *dst)
 	size_t rlen;
 	loff_t nandoff = (spl_spinand_peb_size() * block) + offset;
 
-	return mtd_read(mtd, nandoff, len, &rlen, dst);
+	printf("b: %x, o: %x, d: %08x, l: %x\n", block, offset, (unsigned) dst, len);
+	ret = mtd_read(mtd, nandoff, len, &rlen, dst);
+	printf("mtd read ret = %d, rlen %d\n", ret, (unsigned) rlen);
+
+	       return ret;
+
+	//return mtd_read(mtd, nandoff, len, &rlen, dst);
 }
