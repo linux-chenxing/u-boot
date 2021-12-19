81d0f3055889ed57b4413aee4626a57b15beaa01
Daniel Palmer <daniel@0x0f.com>
2021-12-19 21:25:22 +0900

    ARM: mstar: Add target for SOM2D01

---
+++ board/wirelesstag/som2d01/som2d01.c
@@ -0,0 +1,68 @@
+/*
+ * Mstar Armv7 generic board
+ */
+
+#include <init.h>
+#include <fdtdec.h>
+
+#ifdef CONFIG_DEBUG_UART
+#include <debug_uart.h>
+#endif
+
+#include <common.h>
+#include <asm/u-boot.h>
+#include <spl.h>
+#include <env.h>
+#include <u-boot/crc.h>
+#include <asm/io.h>
+#include <dm.h>
+#include <clk.h>
+#include <ipl.h>
+#include <image.h>
+
+#include "chenxingv7.h"
+
+#include <mstar/board.h>
+
+/* check that some required config options are selected */
+
+#ifndef CONFIG_BOARD_LATE_INIT
+#error "BOARD_LATE_INIT is required"
+#endif
+
+#ifndef CONFIG_DTB_RESELECT
+#error "DTB_RESELECT is required"
+#endif
+
+#ifndef CONFIG_OF_BOARD_SETUP
+#error "OF_BOARD_SETUP is required"
+#endif
+
+#ifndef CONFIG_MULTI_DTB_FIT
+#error "MULTI_DTB_FIT is required"
+#endif
+
+//#ifdef CONFIG_SPL_BUILD
+//#ifndef CONFIG_SPL_LOAD_FIT
+//#error "CONFIG_SPL_LOAD_FIT is required"
+//#endif
+//#endif
+
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_late_init(void){
+	int ret;
+
+	ret = mstar_board_late_init();
+	if(ret)
+		return ret;
+
+	// hacks!
+	// set mmc pinctrl
+	*((u32*)0x1f203c20) = 0x100;
+	*((u32*)0x1f226694) = 0x8; // set secondary sdio gate to clkgen
+	*((u32*)0x1f207114) = 0x18; // degate sdio clock
+
+	return 0;
+}
