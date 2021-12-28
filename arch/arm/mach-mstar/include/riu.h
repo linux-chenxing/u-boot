afed1b531fbe381ab4f2fc6e118c48adfb524a6d
Daniel Palmer <daniel@0x0f.com>
2021-12-28 23:44:40 +0900

    DELETME: usb debugging

--- arch/arm/mach-mstar/include/riu.h
+++ arch/arm/mach-mstar/include/riu.h
@@ -7,12 +7,21 @@ static inline u32 riu_readl(const void* regs, unsigned reg)
 {
 	u32 l = readw(regs + (reg * 2));
 	u32 h = readw(regs + ((reg * 2) + 4));
-	return l | (h << 16);
+
+
+	u32 ret =  l | (h << 16);
+
+	printf("r - 0x%08x - 0x%02x: 0x%08x\n", regs, reg, ret);
+
+	return ret;
 }
 
 static inline void riu_writel(void *regs, unsigned reg, u32 value){
         u16 l = value & 0xffff;
         u16 h = (value >> 16) & 0xffff;
+
+        printf("w - 0x%08x - 0x%02x: 0x%08x\n", regs, reg, value);
+
         writew(l, regs + (reg * 2));
         writew(h, regs + ((reg * 2) + 4));
 }
