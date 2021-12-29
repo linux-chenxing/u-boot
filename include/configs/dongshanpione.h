650077c3be8ce76b69f8fd436033e92182553fe5
Daniel Palmer <daniel@0x0f.com>
2021-12-29 11:52:47 +0900

    ARM: mstar: dongshanpione: Add default environment

--- include/configs/dongshanpione.h
+++ include/configs/dongshanpione.h
@@ -7,4 +7,10 @@
 
 #include "mstarv7.h"
 
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"bootargs=console=ttyS0,115200 clk_ignore_unused "\
+	"ubi.fm_autoconvert=1 ubi.mtd=1 ubi.block=0,rootfs root=/dev/ubiblock0_4\0"\
+	"bootcmd_rescue=ubi readvol ${loadaddr} rescue; bootm ${loadaddr}\0"\
+	"bootcmd=ubi readvol ${loadaddr} kernel; bootm ${loadaddr}\0"
+
 #endif
