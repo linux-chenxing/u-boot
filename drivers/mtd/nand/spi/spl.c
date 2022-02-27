2f01a540192af0edfb3bc1eaef7413d540a779b0
Daniel Palmer <daniel@0x0f.com>
2022-02-27 18:34:45 +0900

    mtd: nand: spi: spl: EINVAL handling
    ubi will try to read past the end of the device.
    Stop retrying if EINVAL comes back.

--- drivers/mtd/nand/spi/spl.c
+++ drivers/mtd/nand/spi/spl.c
@@ -32,8 +32,18 @@ int spl_spinand_read_block(int block, int offset, int len, void *dst)
 
 	for(retries = 0; retries < 10; retries++) {
 		ret = mtd_read(mtd, nandoff, len, &rlen, dst);
-		if(!ret)
+		if (!ret)
 			break;
+		/*
+		 * EINVAL should mean the way we are reading past the end
+		 * of the device and not a timeout or something, so if EINVAL
+		 * comes back don't retry. This happens when the UBI partition
+		 * is configured to use the remaining size of the device instead
+		 * of a fixed size.
+		 */
+		if (ret == -EINVAL)
+			break;
+
 		printf("mtd read ret = %d, rlen %d, retry %d\n", ret, (unsigned) rlen, retries);
 	}
 	return ret;
